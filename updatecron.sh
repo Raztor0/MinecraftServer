c51cbbd7e780dd6e7f5226c5ad99bbd13265ebc5
Razvan Bangu <razvan.bangu@gmail.com>
2016-01-16 00:18:23 -0500

    Autosaving server

--- updatecron.sh
+++ updatecron.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/MinecraftServer/
 git config --global user.email "razvan.bangu@gmail.com"
 git config --global user.name "Razvan Bangu"
-git remote set-url origin git@github.com:Raztor0/MinecraftServer.git
+git remote set-url origin https://github.com/Raztor0/MinecraftServer
 git add .
 git commit -m "Autosaving server"
 git push origin master
