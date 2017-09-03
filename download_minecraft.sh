b1765e7f008df02c6beabed3499f7e79a9818890
Razvan Bangu <raz@razb.me>
2017-09-03 14:11:34 -0400

    Squishing commits

---
+++ download_minecraft.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar
---
+++ run_server.sh
@@ -0,0 +1,1 @@
+screen -dmS minecraft java -Xms2048M -Xmx2048M -jar minecraft_server.1.12.1.jar nogui
